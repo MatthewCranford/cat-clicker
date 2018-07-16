cf432ecd2ff16f075d3758b1c2b0741c8badef86
Matthew Cranford <mcranfordmail@gmail.com>
2018-07-16 14:59:48 -0600

    Add dual cats

--- js/app.js
+++ js/app.js
@@ -1,8 +1,21 @@
-const cat = document.querySelector('.cat');
-const clicker = document.querySelector('.clicker');
-let counter = 0;
+const cat1 = document.querySelector('#cat1');
+const cat2 = document.querySelector('#cat2');
+const clicker1 = document.querySelector('#clicker1');
+const clicker2 = document.querySelector('#clicker2');
+let counter1 = 0;
+let counter2 = 0;
 
-cat.addEventListener('click', () => {
+function catClicker(clicker, counter) {
     counter++;
-    clicker.innerHTML = counter;
+    clicker.innerHTML = `Clicks = ${counter}`;
+    return counter;
+}
+
+cat1.addEventListener('click', function() {
+    counter1 = catClicker(clicker1,counter1);
+    
 });
+cat2.addEventListener('click', function() {
+    counter2 = catClicker(clicker2,counter2);
+});
+
