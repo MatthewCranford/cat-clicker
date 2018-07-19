c17fbc33ea118181e85fce7cd1590f290113aef1
Matthew Cranford <mcranfordmail@gmail.com>
2018-07-19 17:19:25 -0600

    Work on closures

--- js/app.js
+++ js/app.js
@@ -1,43 +1,63 @@
-const cat1 = document.querySelector('#cat1');
-const cat2 = document.querySelector('#cat2');
-const clicker1 = document.querySelector('#clicker1');
-const clicker2 = document.querySelector('#clicker2');
-let counter1 = 0;
-let counter2 = 0;
+// const cat1 = document.querySelector('#cat1');
+// const cat2 = document.querySelector('#cat2');
+// const clicker1 = document.querySelector('#clicker1');
+// const clicker2 = document.querySelector('#clicker2');
+// let counter1 = 0;
+// let counter2 = 0;
 
+// function catClicker(clicker, counter) {
+//     counter++;
+//     clicker.innerHTML = `Clicks = ${counter}`;
+//     return counter;
+// }
 
-const cats = ['../images/2290467335_d4fd0b3bd7_o.jpg','../images/625069434_db86b67df8_b.jpg']
-
-function catClicker(clicker, counter) {
-    counter++;
-    clicker.innerHTML = `Clicks = ${counter}`;
-    return counter;
-}
+const cats = [
+    {
+        name: 'cat1',
+        image: '../images/2290467335_d4fd0b3bd7_o.jpg',
+        count: 0
+    },
+    {
+        name: 'cat2',
+        image: '../images/625069434_db86b67df8_b.jpg',
+        count: 0
+    }
+]
 
 const catList = document.querySelector('#cat-list');
+const catDisplay = document.querySelector('.cat-display');
 const catImg = document.querySelector('.cat-img');
 const catTitle = document.querySelector('.cat-title');
 const catCounter = document.querySelector('.cat-counter');
 
 for (let i = 0; i < cats.length; i++) {
 
-    const elem = document.createElement('li');
-    elem.innerHTML=`Cat${i+1}`
-    catList.append(elem);
-    
-    elem.addEventListener('click', (function(id) {
-        return function() {
-            let counter = 0;
-            catImg.src = cats[id];
-            catTitle.innerHTML = `Cat${i+1}`
-            catCounter.innerHTML = counter;
+    const li = document.createElement('li');
+    const img = document.createElement('img');
+    img.classList.add('cat-img');
+    li.innerHTML=`Cat${i+1}`
+    catList.append(li);
 
-            catImg.addEventListener('click', (function(count) {
+    
+    li.addEventListener('click', (function(id) {
+        return function() {
+            let count = cats[id].count;
+            catDisplay.append(img);
+            img.src = cats[id].image;
+            img.dataset = id;
+            catTitle.innerHTML = `Cat${i+1}`
+            catCounter.innerHTML = count;
+
+            img.addEventListener('click', (function(id) {
                 return function() {
+                    console.log('test')
                     count++;
                     catCounter.innerHTML = count;
+                    cats[id].count += 1;
                 }
-            })(counter));
+                  
+                
+            })(id));
         }
     })(i));
 }
